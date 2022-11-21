2e47e69a85bb65e913af5ada1d24b0da7b3eb86d
Jagdish Padeliya <Jagdishpadaliya983@gmail.com>
2022-11-21 16:32:33 +0530

    21-11-22

--- src/component/SelectedItem/SelectedItem.js
+++ src/component/SelectedItem/SelectedItem.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { AiFillCloseCircle} from "react-icons/ai";
-const SelectedItem = ({backgroundColor,id,color,name,removeOption}) => {
+const SelectedItem = ({backgroundColor,id,mainColor,name,removeOption}) => {
   return (
     <span style={{
-        color:color,
+        color:mainColor,
         backgroundColor:backgroundColor,
         padding:'0.8rem',
         borderRadius:5,
         marginRight:'0.5rem',
         display:"flex",
         justifyContent:"center",
-        alignItems:"center"
+        alignItems:"center",
+        fontWeight:'bold'
       }}
       >
         {name}
